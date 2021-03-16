3678a3c0906640bf8e983b4d4a7dc9fca16e52a9
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-03-16 18:19:07 -0400

    added the functions on index.js, finished adding values to the tables

--- index.js
+++ index.js
@@ -7,22 +7,100 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '',
-    database: 'employee'    
+    database: 'employee'
 });
 
 connection.connect(err => {
-    if(err) throw err;
+    if (err) throw err;
     prompt();
 });
 
 const prompt = () => {
     inquirer
-        .prompt({
-            name: 'action',
+        .prompt([
+        {
+            name: 'choice',
             type: 'list',
             message: 'What would you like to do?',
-            choides: [
-
+            choices: [
+                "View All Employees?",
+                "View All Employee's By Roles?",
+                "View All Emplyee's By Departments",
+                "Update Employee",
+                "Add Employee?",
+                "Add Role?",
+                "Add Department?"
             ]
+        }])
+        .then((value) => {
+            switch (value.choice) {
+                case "View All Employees?":
+                    viewAllEmployees();
+                    break;
+                case "View All Employee's By Roles?":
+                    viewRoles();
+                    break;
+                case "View All Employee's By Departments":
+                    viewDepartments();
+                    break;
+                case "Update Employee":
+                    updateEmployee();
+                    break;
+                case "Add Employee?":
+                    addEmployee();
+                    break;
+                case "Add Role?":
+                    addRole();
+                    break;
+                case "Add Department?":
+                    addDepartment();
+                    break;
+                    
+            }
         })
+};
+
+const viewAllEmployees = () => {
+    connection.query();
+    prompt();
+};
+
+const viewRoles = () => {
+    connection.query();
+    prompt();
+};
+
+const viewDepartments = () => {
+    connection.query();
+    prompt();
+};
+
+const updateEmployee = () => {
+    connection.query();
+    inquirer
+        .prompt([
+
+        ])
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+
+        ])
+};
+
+const addRole = () => {
+    connection.query();
+    inquirer
+        .prompt([
+
+        ])
+};
+
+const addDepartment = () => {
+    inquirer
+        .prompt([
+
+        ])
 };
--- sql/employee.sql
+++ sql/employee.sql
@@ -3,8 +3,8 @@ CREATE DATABASE employee;
 USE employee;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
@@ -19,14 +19,42 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
-INSERT INTO department ()
-VALUE ();
+INSERT INTO department (name)
+VALUE ('Sales');
+INSERT INTO department (name)
+VALUE ('Engineering');
+INSERT INTO department (name)
+VALUE ('Finance');
+INSERT INTO department (name)
+VALUE ('Legal');
 
-INSERT INTO role ()
-VALUE ();
+INSERT INTO role (title, salary, department_id)
+VALUE ('Lead Engineer', 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Accountant', 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Lawyer', 190000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Legal Team Lead', 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Software Engineer', 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ('Sales Lead', 100000, 1);
 
-INSERT INTO employee ()
-VALUE ();
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Yigit Eren', 'Ozgur', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Sommer', 'Moya-Mendez', 2 , 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Eric', 'Hurley', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Haleigh', 'Spurlock', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Sam', 'Finn', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Natasha', 'Stark', 3, 1);
